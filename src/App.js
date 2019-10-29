5b412d1535d0bbfbab00ed12615ab15e46a7095d
Oblum1989 <oscar.blum@gmail.com>
2019-10-29 00:39:10 -0500

    repeater is added

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      repeater : ""
+    }
+    this.repeatText = this.repeatText.bind(this)
+  }
+
   render() {
     return (
       <div>
         <h1>Un repetidor</h1>
-        <input type="text" placeholder="Empieza a escribir algo" />
-        <p className="repeater"></p>
+        <input type="text" placeholder="Empieza a escribir algo" onChange={this.repeatText} />
+        <p className="repeater">{this.state.repeater}</p>
       </div>
     );
+  }
+
+  repeatText(e){
+    this.setState({
+      repeater : e.target.value
+    })
   }
 }
 
